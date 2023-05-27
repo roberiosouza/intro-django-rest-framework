c8e16f23afcd91fb93ad7bacae5ba56f3c20af76
Robério <rroberiosouza@gmail.com>
2023-05-27 20:35:36 -0300

    Implemetado outros verbos
    Implementado verbos de 'PUT' e 'DELETE'

--- api_todo/app/urls.py
+++ api_todo/app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from app.views import todo_list
+from app.views import todo_list, todo_detail_change_and_delete
 
 urlpatterns = [
-    path('', todo_list)
+    path('', todo_list),
+    path('<int:pk>/', todo_detail_change_and_delete),
 ]
