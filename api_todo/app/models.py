1c1d4135f111d8a365607c335a45640168fe2dc6
Robério <rroberiosouza@gmail.com>
2023-05-27 19:33:28 -0300

    Criado modelo Todo
    Criado modelo e migration 'Todo'.

--- api_todo/app/models.py
+++ api_todo/app/models.py
@@ -1,3 +1,8 @@
+from xml.etree.ElementInclude import default_loader
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    name = models.CharField(max_length=120)
+    done = models.BooleanField(default=False)
+    create_at = models.DateTimeField(auto_now_add=True)
